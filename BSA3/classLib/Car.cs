5a2a4fe2c499e17d3a4079a0dfd12fc9b498c878
Kirill <makfa78@gmail.com>
2018-05-14 19:42:07 +0300

    Add project

---
+++ BSA3/classLib/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace classLib
+{
+	public enum carType
+    {
+        passanger,
+        truck,
+        bus,
+        motorcycle
+    }
+	public class Car
+	{
+
+		public uint currentID { get; private set; }
+
+		public carType currentType { get; private set; }
+
+		public float currentBalance { get; private set; }
+
+		public Car(uint id, carType type, float balance)
+		{
+			currentID = id;
+			currentType = type;
+			currentBalance = balance;
+		}
+
+		public float addToBalance(float someMoney)
+		{
+			currentBalance += someMoney;
+			return currentBalance;
+		}
+
+		public float removeFromBalance(float someMoney)
+		{
+			currentBalance -= someMoney;
+			return currentBalance;
+		}
+	}
+}
+
+ 
